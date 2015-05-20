6c32886143c698eb2ff8b867f32824680b844821
Paul Gonzalez <pjg5005@gmail.com>
2015-05-20 14:59:48 -0500

    Converts user input to integer, prints from 1 to the users input

--- challenge_1.rb
+++ challenge_1.rb
@@ -1,6 +1,14 @@
 # FIZZ BUZZ
+# Let's make a digital version of the FizzBuzz game.  Write a program that prints the
+# numbers from 1 to a given number. But for multiples of three print “Fizz”
+# instead of the number and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz”."
+
 
 puts "Let's play Fizz Buzz. Please give us a number:"
-number = gets.chomp
+user_input = gets.chomp
 
-puts number
+(1..user_input.to_i).each do |number|
+  puts number
+end
+
