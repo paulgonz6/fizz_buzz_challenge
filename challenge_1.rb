4d8182b38a5d3821a12dc8f640323598d1e0e40f
Paul Gonzalez <pjg5005@gmail.com>
2015-05-20 20:03:12 -0500

    Adds another idea for FizzBuzz

--- challenge_1.rb
+++ challenge_1.rb
@@ -4,18 +4,33 @@
 # instead of the number and for the multiples of five print “Buzz”.
 # For numbers which are multiples of both three and five print “FizzBuzz”."
 
+# puts "Let's play Fizz Buzz. Please give us a number:"
+# user_input = gets.chomp
 
-puts "Let's play Fizz Buzz. Please give us a number:"
-user_input = gets.chomp
+class Game
 
-(1..user_input.to_i).each do |number|
-  if number % 15 == 0
-    puts "FizzBuzz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  elsif number % 3 == 0
-    puts "Fizz"
-  else
-    puts number
+  attr_accessor :player, :number
+
+  def initialize(player, number)
+    @player = player
+    @number = number.to_i
   end
+
+  def fizzbuzz
+    (1..number).each do |number|
+      if number % 15 == 0
+        puts "FizzBuzz"
+      elsif number % 5 == 0
+        puts "Buzz"
+      elsif number % 3 == 0
+        puts "Fizz"
+      else
+        puts number
+      end
+    end
+  end
+
 end
+
+g = Game.new("Daniel", 150)
+g.fizzbuzz
